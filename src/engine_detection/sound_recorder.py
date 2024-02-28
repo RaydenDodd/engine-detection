82f507886fb52cfc499fca2601ac35ab60347e69
Dylan Green <dgpokemaster08@gmail.com>
2024-02-28 02:16:53 -0500

    Added sound recorder class and gui class starter

---
+++ src/engine_detection/sound_recorder.py
@@ -0,0 +1,62 @@
+import sounddevice as sd
+import librosa
+from pydub import AudioSegment
+from scipy.io.wavfile import write
+
+SAMPLE_RATE = 48000
+NUM_CHANNELS = 2
+
+
+class SoundRecorder:
+
+    def __init__(self):
+        self.device_id = -1
+
+    def set_device(self, index: int):
+        if index < 0 or index >= len(sd.query_devices()):
+            raise IndexError('Device index out of range')
+        else:
+            self.device_id = index
+
+    def record(self, duration: int):
+        if self.device_id == -1:
+            raise ValueError('Device index not set')
+
+        sd.default.device = self.device_id
+        recording = sd.rec(int(duration * SAMPLE_RATE), samplerate=SAMPLE_RATE, channels=NUM_CHANNELS)
+        sd.wait()
+        return recording
+
+    @staticmethod
+    def get_devices() -> list[str]:
+        devices = sd.query_devices()
+        names = list()
+
+        for device in devices:
+            names.append(device['name'])
+
+        return names
+
+    @staticmethod
+    def preprocess(audio, num_channels, sample_rate):
+        data_type = 'ndarray'
+
+        if sample_rate != SAMPLE_RATE:
+            audio = SoundRecorder.__resample(audio, sample_rate)
+
+        if num_channels != NUM_CHANNELS:
+            write('temp.wav', SAMPLE_RATE, audio)
+            left = AudioSegment.from_wav('temp.wav')
+            right = AudioSegment.from_wav('temp.wav')
+            audio = AudioSegment.from_mono_audiosegments(left, right)
+            data_type = 'audiosegment'
+
+        # Write the processed audio to a file
+        if data_type == 'ndarray':
+            write('output.wav', SAMPLE_RATE, audio)
+        else:
+            audio.export('output.wav', format='wav')
+
+    @staticmethod
+    def __resample(audio, orig_sample_rate):
+        return librosa.resample(audio, orig_sr=orig_sample_rate, target_sr=SAMPLE_RATE)
